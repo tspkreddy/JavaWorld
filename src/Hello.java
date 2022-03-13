f16479e0333ce9d77759c369afd87aeb9b5aff3f
tspkreddy <101514576+tspkreddy@users.noreply.github.com>
2022-03-13 23:12:41 +0530

    Update Hello.java
    testing with comments

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for understanding darling
+// Just for understanding darling dears
         System.out.println("This is my first github commit codes");
 
 
