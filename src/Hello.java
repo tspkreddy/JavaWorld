eb9a1ce2cd4bbd103f4921f9fabcf846a464c8ec
Sri Pavan Kishore Reddy Tamanampudi <101514576+tspkreddy@users.noreply.github.com>
2022-03-13 23:17:04 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for understanding darling dears cool
+// Just for understanding darling dears cools 
         System.out.println("This is my first github commit codes");
 
 
