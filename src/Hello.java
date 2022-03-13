ee6db1352763f009ed357a66713db0f28341dbcd
tspkreddy <101514576+tspkreddy@users.noreply.github.com>
2022-03-13 23:13:41 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for understanding darling dears
+// Just for understanding darling dears cool
         System.out.println("This is my first github commit codes");
 
 
