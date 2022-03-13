b16f8a4a1cd2f2e4f70f29543c29426fba4fc363
tspkreddy <101514576+tspkreddy@users.noreply.github.com>
2022-03-13 23:10:19 +0530

    Update Hello.java
    added darling in comment

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for understanding
+// Just for understanding darling
         System.out.println("This is my first github commit codes");
 
 
