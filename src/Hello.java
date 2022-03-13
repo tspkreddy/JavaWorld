1e99f17da6c6d4692fdeda95b28f06eb877ab81c
Sri Pavan Kishore Reddy Tamanampudi <tspkreddy@users.noreply.github.com>
2022-03-13 23:30:08 +0530

    First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for 
+// Just for funny   cool
         System.out.println("This is my first github commit codes");
 
         System.out.print("changed");
