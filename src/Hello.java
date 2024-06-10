eccd1710c09ab31d7fc85aed0db8735709e584f8
RupaMedapati <rupabhavanimedapati@gmail.com>
2024-06-10 11:23:42 +0530

    for recap of github helping rupa

--- src/Hello.java
+++ src/Hello.java
@@ -2,11 +2,9 @@ public class Hello {
 
     public static void main(String[] arguments)
     {
-// Just for funny   cool
-        System.out.println("This is my first github commit codes");
 
-       // System.out.print("changed");88
-        
-       // changes first in GitHub77hh
+        System.out.println("This is for helping rupa");
+
+
 
     }}
